83209b07207ecb158da227cd8166770a660afcd7
zeen <zhl146@gmail.com>
2022-07-22 08:49:42 -0400

    blow it all up and use next

--- ui/src/components/Logo.js
+++
@@ -1,9 +0,0 @@
-import { Box } from "@mui/material"
-
-export default function Logo() {
-  return (
-    <Box sx={{ height: "65px", width: "65px" }}>
-      <img src="https://s.runpod.io/image/logo-notext.png" alt="runpod logo"></img>
-    </Box>
-  )
-}
