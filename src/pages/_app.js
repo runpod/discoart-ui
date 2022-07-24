c183e657dfcae25acc415d77cca02ff96534b066
zhl146 <zhl146@gmail.com>
2022-07-24 07:03:47 +0000

    add toolbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,36 @@
 // import '../styles/globals.css'
 import ThemeProvider from "./theme"
+import * as React from "react"
+import AppBar from "@mui/material/AppBar"
+import Toolbar from "@mui/material/Toolbar"
+import Button from "@mui/material/Button"
+import { Container, Stack } from "@mui/material"
+import { useRouter } from "next/router"
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  const route = router.pathname
+
   return (
     <ThemeProvider>
+      <AppBar position="static">
+        <Toolbar>
+          <Container maxWidth="xl">
+            <Stack direction="row" spacing={2}>
+              <Button disabled={route === "/"} href="/" color="inherit">
+                Create
+              </Button>
+              <Button disabled={route === "/queue"} href="/queue" color="inherit">
+                Queue
+              </Button>
+              <Button disabled={route === "/gallery"} href="/gallery" color="inherit">
+                Gallery
+              </Button>
+            </Stack>
+          </Container>
+        </Toolbar>
+      </AppBar>
       <Component {...pageProps} />
     </ThemeProvider>
   )
