7fecf90ed7f4df957eb3fa4b305e9a6b9001c73f
zhl146 <zhl146@gmail.com>
2022-07-28 15:17:02 +0000

    fix logout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import AppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
 import Button from "@mui/material/Button"
-import { Container, Stack, Typography } from "@mui/material"
+import { Container, Stack } from "@mui/material"
 import { useRouter } from "next/router"
 import { SWRConfig } from "swr"
 import axios from "axios"
@@ -15,15 +15,12 @@ import { DarkTheme } from "../theme"
 const fetcher = (url) => axios.get(url).then((res) => res.data)
 
 function MyApp({ Component, pageProps }) {
-  const [cookies, setCookie, removeCookie] = useCookies(["password"])
-
   const router = useRouter()
 
   const route = router.pathname
 
   const handleLogout = () => {
-    removeCookie("password")
-    router.replace("/")
+    fetch("/api/logout").then(() => router.replace("/"))
   }
 
   return (
---
+++ src/pages/api/logout.js
@@ -0,0 +1,19 @@
+import Cookies from "cookies"
+
+const handler = async (req, res) => {
+  try {
+    const cookies = new Cookies(req, res)
+    // Get a cookie
+    cookies.set("password", { maxAge: 0 })
+
+    res.status(200).json({
+      success: true,
+    })
+  } catch (e) {
+    res.status(200).json({
+      success: false,
+    })
+  }
+}
+
+export default handler
