88168bb68bb74b8952714821158e4614156db8da
zhl146 <zhl146@gmail.com>
2022-07-29 21:26:17 +0000

    make files password protected

--- src/pages/api/image/[folder]/[file].js
+++ src/pages/api/image/[folder]/[file].js
@@ -1,6 +1,8 @@
 const path = require("path")
 import express from "express"
 
+import { getAuth } from "@utils/getAuth"
+
 export const config = {
   api: { externalResolver: true },
 }
@@ -8,6 +10,22 @@ export const config = {
 const handler = express()
 
 const serveFiles = express.static("/workspace/out")
+
+handler.use(["/api/image/", "/image"], async (req, res, next) => {
+  try {
+    const { loggedIn } = await getAuth({ req, res })
+
+    if (loggedIn) {
+      next()
+    } else {
+      res.status(401).end()
+      return
+    }
+  } catch (e) {
+    res.status(401).end()
+    return
+  }
+})
 
 handler.use(["/api/image/", "/image"], serveFiles)
 
